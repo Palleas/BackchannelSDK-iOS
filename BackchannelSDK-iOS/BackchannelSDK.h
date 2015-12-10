fd2562104917a2ce7b24a3561398765845574dbd
Romain Pouclet <rpouclet@mirego.com>
2015-12-10 17:50:38 -0500

    Setup Carthage

---
+++ BackchannelSDK-iOS/BackchannelSDK.h
@@ -0,0 +1,19 @@
+//
+//  BackchannelSDK-iOS.h
+//  BackchannelSDK-iOS
+//
+//  Created by Romain Pouclet on 2015-12-10.
+//  Copyright © 2015 Backchannel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BackchannelSDK-iOS.
+FOUNDATION_EXPORT double BackchannelSDK_iOSVersionNumber;
+
+//! Project version string for BackchannelSDK-iOS.
+FOUNDATION_EXPORT const unsigned char BackchannelSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BackchannelSDK_iOS/PublicHeader.h>
+
+
